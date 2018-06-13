ad203dc2d7394cd4c3ebdb48d383b79954c518ff
Dmitry Mikhaylenko <dmikhaylenko@hotmail.com>
2018-06-13 16:25:34 +0400

    Base commit

---
+++ src/containers/App/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">App Container</div>
+    );
+  }
+}
+
+export default App;
